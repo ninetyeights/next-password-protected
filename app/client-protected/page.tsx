d8f20340674b41483a7de1193a16f5c94b0ae7dc
Rain <78558736+ninetyeights@users.noreply.github.com>
2024-05-12 14:33:37 -0400

    update unprotected page

--- app/client-protected/page.tsx
+++ app/client-protected/page.tsx
@@ -13,13 +13,12 @@ export default function ClientProtected() {
 
     return (
         <main className="flex justify-center items-center flex-col min-h-screen bg-purple-200">
-            <p>This content is unprotected!</p>
             {status === 'authenticated' ?
                 <>
-                    <h1 className="font-semibold text-3xl my-4">🔒️ Protected page - Client</h1>
-                    <p>This page is password protected!</p>
+                    <h1 className="font-semibold text-3xl my-4 -mt-32">🔒️ Protected page - Client</h1>
+                    <p className="mb-4">This page is password protected!</p>
                 </>
-                : <a href="/api/auth/signin?callbackUrl=/client-protected" className="bg-gray-900 text-white hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Sign In</a>}
+                : <a href="/api/auth/signin?callbackUrl=/client-protected" className="bg-gray-900 text-white hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium -mt-32 mb-4">Sign In</a>}
             <p>This content is unprotected!</p>
         </main>
     )
