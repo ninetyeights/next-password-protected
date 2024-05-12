f2045bc70499d3fc086988eaf3bba2a9dbfc6d01
Rain <78558736+ninetyeights@users.noreply.github.com>
2024-05-12 02:09:22 -0400

    update unprotected page

--- app/unprotected/page.tsx
+++ app/unprotected/page.tsx
@@ -2,7 +2,7 @@ export default async function Protected() {
     return (
         <main className="flex justify-center items-center flex-col min-h-screen bg-sky-200">
             <h1 className="font-semibold text-3xl mb-10 -mt-32">🌎 Unprotected page</h1>
-            <p>This page is password protected!</p>
+            <p>This page is unprotected!</p>
         </main>
     )
 }
